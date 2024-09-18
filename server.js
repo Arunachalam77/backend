fc842ac4e3b250d540f8c29ca2a9b481c7127de8
Arunachalam <arunachalam@crayond.co>
2024-09-18 16:02:06 +0530

    add env for port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import express from "express";
 import routes from "./routes/index.js"; 
 
 const app = express();
-const PORT = process.env.PORT ;
+const PORT = process.env.PORT || 4000 ;
 
 app.use(express.json());
 app.use("/", routes);
