f27bfd388c18d0530a0403e683925a6a1e7ef270
Arunachalam <arunachalam@crayond.co>
2024-09-18 15:46:57 +0530

    Remove node_modules from version control and update .gitignore

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
+import 'dotenv/config'
 import express from "express";
 import routes from "./routes/index.js"; 
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT ;
 
 app.use(express.json());
 app.use("/", routes);
